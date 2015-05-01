fae229cf7c53af84e2bd779888b753da1d577214
johnvictor <johnvictor.v@cognizant.com>
2015-05-01 23:23:28 +0530

    static get-vegetables service from json

--- src/main/java/com/achilles/controller/VegetablesController.java
+++ src/main/java/com/achilles/controller/VegetablesController.java
@@ -1,38 +1,43 @@
 package com.achilles.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.achilles.model.Vegetable;
 
 @RestController
 public class VegetablesController {
 
 	@RequestMapping(value = "/get-vegetables")
-	public @ResponseBody List<Vegetable> getVegetables() {
-		
-		List<Vegetable> vegetableList = new ArrayList<Vegetable>();
-		
-		Vegetable onion = new Vegetable();
-		onion.setName("Onion");
-		onion.setThumb_path("/onion.jpg");
-		
-		Vegetable carrot = new Vegetable();
-		carrot.setName("Carrot");
-		carrot.setThumb_path("/carrot.jpg");
-		
-		Vegetable brinjal = new Vegetable();
-		brinjal.setName("Brinjal");
-		brinjal.setThumb_path("/brinjal.jpg");
-		
-		vegetableList.add(onion);
-		vegetableList.add(carrot);
-		vegetableList.add(brinjal);
-		
-		return vegetableList;
+	public @ResponseBody String getVegetables() {
+
+		String vegetablesList = readFileFromClasspath("static/json/get-vegetables.json");
+
+		return vegetablesList;
+	}
+
+	public String readFileFromClasspath(final String fileName) {
+
+		StringBuilder result = new StringBuilder("");
+
+		// Get file from resources folder
+		InputStream inputStream = ClassLoader
+				.getSystemResourceAsStream(fileName);
+		StringWriter writer = new StringWriter();
+		String encoding = "UTF-8";
+		try {
+			IOUtils.copy(inputStream, writer, encoding);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String theString = writer.toString();
+
+		return theString;
+
 	}
 }
